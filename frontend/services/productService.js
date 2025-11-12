fa619592ed009f2ae93673a2660529e624d2d5d0
Sumeet Chauhan <schauhan8886@gmail.com>
2025-11-12 19:48:11 +0530

    Add API link

--- frontend/services/productService.js
+++ frontend/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/products";
+const API_URL = "https://abyssx-mvp-backend.onrender.com";
 
 export const getProducts = async () => {
   const res = await axios.get(API_URL);
